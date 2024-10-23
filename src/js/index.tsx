4dbe3e7b247404c65983bbd7b7d59c2ec66b4967
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2024-10-24 01:43:13 +0900

    chore: 仮のコンポーネントを複数作成しました

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import TodoItem from "../js/components/TodoItem";
+import TodoList from "../js/components/TodoList";
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
-// Render your React component instead
 const root = createRoot(document.getElementById("app")!);
-root.render(<h1>Hello, world</h1>);
+root.render(<TodoList />);
